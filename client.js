b7aa6d30c06d25b1dc5fd4a0f8cb40c9ad9ff444
pierre portal <pierreportal.mac@gmail.com>
2019-09-29 15:21:25 +0200

    chalk

--- client.js
+++ client.js
@@ -8,34 +8,35 @@ let username = null
 // CONNECT SOCKET
 socket.on('connect', () => {
     console.clear()
-    console.log(chalk.inverse('start chatting !'))
+    process.stdout.write(chalk.inverse('start chatting !\n'))
     const [, , name] = process.argv
     username = name
+    socket.send({ type: 'program', msg: 'is connected', username })
 });
 
 
 
 // RECEIVE MSG
 socket.on('message', data => {
-
-    const { msg, username } = data
-
-    console.log(chalk.italic.magenta(username + ' ~'), msg.split('\n')[0])
+    const { type, msg, username } = data
+    const res = chalk.italic.magenta(username + ' ~ ') + msg.split('\n')[0]
+    type === 'program' ? console.log(chalk.inverse.magenta(res)) : console.log(res)
+    // console.log('--->', chalk.italic.magenta(username + ' ~'), msg.split('\n')[0])
 });
 
 // DISCONNECT
 socket.on('disconnect', () => {
-
     socket.emit('disconnect')
+    // socket.send({ type: 'program', msg: 'left', username })
     // console.log('OUPS')
 });
 
 // START PROMPT
 
 repl.start({
-    promp: '',
+    prompt: '',
     eval: msg => {
         socket.send({ msg, username })
-        console.clear()
+        // console.clear()
     }
 })
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 
-
 const http = require('http').createServer((req, res) => {
     res.write('connected');
     res.end();
@@ -11,16 +10,21 @@ const io = require('socket.io')(http)
 http.listen(process.env.PORT, () => console.log('listening on port 3000'));
 
 
-
 io.on('connection', socket => {
+
     console.log('connection...');
+
     socket.on('message', data => {
+
         socket.broadcast.emit('message', data);
+
     });
 });
 
 
 
+
 io.on('disconnect', () => {
+
     console.log('disconnection.')
 });
